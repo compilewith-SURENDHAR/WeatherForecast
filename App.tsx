b15d9f040f6f011f06b32e8d3aa6f59385ef8a37
surendhar <surendhar010104@gmail.com>
2023-10-24 13:56:13 +0530

    current weather created basic

--- App.tsx
+++ App.tsx
@@ -1,14 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView style= {styles.wrapper}>
+      <View style = {styles.container}>
+        <Text style = {styles.temp}>21</Text>
+        <Text style = {styles.feels}>feels like 15</Text>
+        <View style = {styles.highLow}>
+          <Text style = {styles.highLowText}>high: 26</Text>
+          <Text style = {styles.highLowText}>low: 13</Text>
+        </View>
+      </View>
+      <View style = {styles.footer}>
+          <Text style = {styles.footerText}>its a clear weather!</Text>
+        </View>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  wrapper : {
+    flex : 1,
+    backgroundColor : 'lightblue',
+  },
+  container : {
+    flex : 1,
+    marginBottom : 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+    //borderWidth : 3,
+    //bordercolor : 'black'
+  },
+  temp : {
+    fontSize : 60,
+    color : 'darkblue',
+    textAlign : 'center'
+  },
+  feels : {
+    fontSize : 30,
+    color : 'black',
+    textAlign : 'center'
+  },
+  highLow : {
+    flexDirection : 'row',
+  },
+  highLowText : {
+    fontSize : 22,
+    color : 'black',
+    padding : 4
+  },
+  footer : {
+    justifyContent : 'flex-end',
+    alignItems : 'center',
+    paddingBottom : 30,
+  },
+  footerText : {
+    fontSize : 30,
+    color : 'red'
+  }
+})
 
 
